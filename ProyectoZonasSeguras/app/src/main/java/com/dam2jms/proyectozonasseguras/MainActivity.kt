2d4b9f0da1b6368626ea9aa42fcdc4fa92944b47
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-12-18 12:15:17 +0100

    commit

--- ProyectoZonasSeguras/app/src/main/java/com/dam2jms/proyectozonasseguras/MainActivity.kt
+++ ProyectoZonasSeguras/app/src/main/java/com/dam2jms/proyectozonasseguras/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.dam2jms.proyectozonasseguras
 
+import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,9 +19,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dam2jms.proyectozonasseguras.ui.theme.ProyectoZonasSegurasTheme
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.DefaultIndoorStateChangeListener
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.IndoorStateChangeListener
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
@@ -46,22 +54,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
-    var properties by remember {
-        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
-    }
+    GoogleMap (modifier = Modifier.fillMaxSize()){
 
-    Box(Modifier.fillMaxSize()) {
-        GoogleMap(
-            modifier = Modifier.matchParentSize(),
-            properties = properties,
-            uiSettings = uiSettings
-        )
-        Switch(
-            checked = uiSettings.zoomControlsEnabled,
-            onCheckedChange = {
-                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
-            }
-        )
     }
+}
+
+@Composable
+fun GoogleMap(
+    mergeDescendants: Boolean = false,
+    modifier: Modifier = Modifier,
+    cameraPositionState: CameraPositionState = rememberCameraPositionState(),
+    contentDescription: String? = null,
+    googleMapOptionsFactory: () -> GoogleMapOptions = { GoogleMapOptions() },
+    properties: MapProperties = DefaultMapProperties,
+    locationSource: LocationSource? = null,
+    uiSettings: MapUiSettings = DefaultMapUiSettings,
+    indoorStateChangeListener: IndoorStateChangeListener = DefaultIndoorStateChangeListener,
+    onMapClick: (LatLng) -> Unit? = null,
+    onMapLongClick: (LatLng) -> Unit? = null,
+    onMapLoaded: () -> Unit? = null,
+    onMyLocationButtonClick: () -> Boolean? = null,
+    onMyLocationClick: (Location) -> Unit? = null,
+    onPOIClick: (PointOfInterest) -> Unit? = null,
+    contentPadding: PaddingValues = NoPadding,
+    content: @Composable () -> Unit? = null
+){
+
 }
